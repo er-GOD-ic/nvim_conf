a66e149b155b6eeffd5f177e4a8556dab12f70e4
er-GOD-ic <kamepower@outlook.jp>
2025-10-02 20:59:44 +0900

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { 
+                "lua",
+                "c",
+                "cpp",
+                "rust",
+                "python",
+                "bash",
+                "markdown",
+                "markdown-inline",
+                "java",
+                "html",
+                "javascript",
+                "php",
+                "toml",
+                "gitignore",
+                "json",
+                "hjson",
+            },
+            sync_install = true,
+            auto_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
