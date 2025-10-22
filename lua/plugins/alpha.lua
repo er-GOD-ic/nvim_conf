29e891fd85d8fee1fe5dd53d7040572ddf949dbc
er-GOD-ic <kamepower@outlook.jp>
2025-10-22 22:13:03 +0900

    add alpha(dashboard)

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,39 @@
+return {
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", "<cmd>ene <CR>"),
+            dashboard.button("f", "󰈞  Find file", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("h", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
+            dashboard.button("g", "󰈬  Find word", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+        }
+
+        alpha.setup(dashboard.config)
+    end,
+}
