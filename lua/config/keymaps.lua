46115807843431ed98d382d6edd3a5ad31e80f1f
er-GOD-ic <kamepower@outlook.jp>
2025-11-09 13:15:34 +0900

    keymapにターミナル操作を追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,11 @@
 -- save
-vim.keymap.set("n", "<leader>w", ":w\n", { noremap = true, silent = true, desc = "save current buffer" })
-vim.keymap.set("n", "<leader>W", ":wa\n", { noremap = true, silent = true, desc = "save all buffers" })
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, silent = true, desc = "save current buffer" })
+vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { noremap = true, silent = true, desc = "save all buffers" })
 -- quit
-vim.keymap.set("n", "<leader>q", ":q\n", { noremap = true, silent = true, desc = "close current buffer" })
-vim.keymap.set("n", "<leader>Q", ":qa\n", { noremap = true, silent = true, desc = "quit neovim" })
-vim.keymap.set("n", "<leader>x", ":x\n", { noremap = true, silent = true, desc = "save and close current buffer" })
-vim.keymap.set("n", "<leader>X", ":xa\n", { noremap = true, silent = true, desc = "save and quit neovim" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true, silent = true, desc = "close current buffer" })
+vim.keymap.set("n", "<leader>Q", "<cmd>qa<cr>", { noremap = true, silent = true, desc = "quit neovim" })
+vim.keymap.set("n", "<leader>x", "<cmd>x<cr>", { noremap = true, silent = true, desc = "save and close current buffer" })
+vim.keymap.set("n", "<leader>X", "<cmd>xa<cr>", { noremap = true, silent = true, desc = "save and quit neovim" })
 
 -- move focus
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
@@ -28,7 +28,6 @@ vim.keymap.set("n", "<A-l>", resize.ResizeRight, { noremap = true, silent = true
 -- clear search
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch\n", { noremap = true, silent = true, desc = "clear search" })
 
--- open term
-vim.keymap.set("n", "<leader>t", "<cmd>term<CR>a", { noremap = true, silent = true, desc = "open terminal" })
--- escape term
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "escape terminal" })
+-- terminal
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true, desc = "toggle terminal" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "escape terminal" })
