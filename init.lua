f904110cbbdabe71c41c52c5606f9e3e27faeaa8
er-GOD-ic <kamepower@outlook.jp>
2025-11-09 13:16:56 +0900

    initにcustom-hlを反映

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("config.options") -- editor options
 require("config.keymaps") -- general keymaps
 require("config.ime-handler") -- ime handler
+require("config.custom-hl") -- custom highlight
 require("config.lazy") -- plugin manager
 
