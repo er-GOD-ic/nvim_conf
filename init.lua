ac594d535e95dae7a81fdef83abea99b4a09e987
er-GOD-ic <kamepower@outlook.jp>
2025-11-28 21:56:43 +0900

    rename custom-hl.lua > hml-hl.lua, and and local-optins.lua in init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-require("config.options") -- editor options
+require("config.options") -- editor global options
+require("config.local-options") -- handle local options in specific filetype
 require("config.keymaps") -- general keymaps
 require("config.ime-handler") -- ime handler
-require("config.custom-hl") -- custom highlight
+require("config.hml-hl") -- H, M, L highlight
 require("config.lazy") -- plugin manager
 
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,28 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = false,
-			custom_highlights = function(colors)
-				return {
-                    CursorLineNr = { bg = "#2a2b3d" },
-					CursorColumn = { bg = "#2a2b3d" },
-				}
-			end,
-		})
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.opt.guicursor = {
+            "n:block-Cursor",
+            "v:block-CursorVisual",
+            "c:block-Cursor",
+            "i:ver25-Cursor",
+        }
 
-		vim.cmd.colorscheme("catppuccin")
-	end,
+        require("catppuccin").setup({
+            flavour = "mocha",
+            transparent_background = false,
+
+            custom_highlights = function(colors)
+                return {
+                    CursorVisual = { bg = colors.mauve },
+                    CursorLineNr = { bg = "#2a2b3d" },
+                    CursorColumn = { bg = "#2a2b3d" },
+                }
+            end,
+        })
+
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
