fa4d3d78ddb4d4c5e79366837ad621952007af5a
er-GOD-ic <kamepower@outlook.jp>
2025-12-10 18:49:29 +0900

    config some

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,17 +3,8 @@ return {
     version = "*",
     config = function()
         require("toggleterm").setup({
+            use_libuv_file_watcher = true,
             size = 13,
         })
-
-        vim.keymap.set("t", "<CR>", function()
-            -- まず Enter を端末へ送る（通常の動作）
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
-
-            -- Neo-tree を非同期で更新
-            vim.schedule(function()
-                vim.cmd("Neotree refresh")
-            end)
-        end, { silent = true })
     end,
 }
