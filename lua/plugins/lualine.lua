b3e47413ef534c9aa7401fe5b80739948539db35
er-GOD-ic <kamepower@outlook.jp>
2026-03-01 22:26:56 +0900

    show macro status on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,13 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("lualine").setup({ options = { theme = "nightfly" } })
+        local MacroStatus = require("config.lualine-macro")
+        require("lualine").setup({
+            options = { theme = "nightfly" },
+            sections = {
+                lualine_b = { "filename", "diff" },
+                lualine_c = { MacroStatus },
+            },
+        })
     end,
 }
