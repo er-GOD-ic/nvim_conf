8af8df964bb6d9c4c55a90f49374a5186eab67fc
ergodic <kamepower@outlook.jp>
2026-04-13 17:06:51 +0900

    add copilot to md

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,6 +4,9 @@ return {
     event = "InsertEnter",
     config = function()
         require("copilot").setup({
+            filetypes = {
+               markdown = true,
+            },
             suggestion = {
                 enabled = true,
                 auto_trigger = true, -- ← 入力中に自動表示
