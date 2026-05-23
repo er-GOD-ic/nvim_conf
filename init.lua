50d9e43a7a3e09ca2636cb8e7dca2673019540da
er-GOD-ic <kamepower@outlook.jp>
2026-05-24 00:24:25 +0900

    separate hml-hl to plugin

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require("config.options")       -- editor global options
 require("config.local-options") -- handle local options in specific filetype
 require("config.keymaps")       -- general keymaps
 require("config.ime-handler")   -- ime handler
-require("config.hml-hl")        -- H, M, L highlight
 require("config.count-chars")
 require("config.lazy")          -- plugin manager
 
---
+++ lua/plugins/hml.lua
@@ -0,0 +1,17 @@
+return {
+    "er-GOD-ic/hml.nvim",
+    -- dir = "~/.config/nvim-plugins/hml.nvim",
+    opts = {
+        highlight = {
+            h = "Directory",
+            m = "Directory",
+            l = "Directory",
+        },
+
+        signs = {
+            h = " ",
+            m = " ",
+            l = " ",
+        },
+    },
+}
