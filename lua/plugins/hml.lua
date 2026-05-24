67e7d3584831f44f430e0a578688a4899ea0b7d3
er-GOD-ic <kamepower@outlook.jp>
2026-05-24 12:45:13 +0900

    change some opts

--- lua/plugins/hml.lua
+++ lua/plugins/hml.lua
@@ -1,17 +1,5 @@
 return {
     "er-GOD-ic/hml.nvim",
     -- dir = "~/.config/nvim-plugins/hml.nvim",
-    opts = {
-        highlight = {
-            h = "Directory",
-            m = "Directory",
-            l = "Directory",
-        },
-
-        signs = {
-            h = "",
-            m = "",
-            l = "",
-        },
-    },
+    opts = {},
 }
