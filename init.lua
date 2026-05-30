61aadf2c675982f8aee5a011dcefef0dab699b34
er-GOD-ic <kamepower@outlook.jp>
2026-05-30 21:05:22 +0900

    Changes related to the Pig Plugin

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
+vim.g.original_pwd = vim.fn.getcwd()
+
 require("config.options")       -- editor global options
 require("config.local-options") -- handle local options in specific filetype
 require("config.keymaps")       -- general keymaps
 require("config.ime-handler")   -- ime handler
 require("config.count-chars")
 require("config.lazy")          -- plugin manager
-
