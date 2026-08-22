e69251b53f9e153e935385af22318302ee1042a4
ergodice <kamepower@outlook.jp>
2026-08-22 23:36:11 +0900

    add smart split

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,36 +1,23 @@
--- reload config
-vim.keymap.set('n', '<leader>rs', ':luafile $MYVIMRC<CR>', { desc = "Reload Config" })
--- save
+-- save/quit
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, silent = true, desc = "save current buffer" })
-vim.keymap.set("n", "<leader>W", "<cmd>wa<cr>", { noremap = true, silent = true, desc = "save all buffers" })
--- quit
+vim.keymap.set("n", "<leader><leader>w", "<cmd>wa<cr>", { noremap = true, silent = true, desc = "save all buffers" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true, silent = true, desc = "close current buffer" })
-vim.keymap.set("n", "<leader>Q", "<cmd>qa<cr>", { noremap = true, silent = true, desc = "quit neovim" })
+vim.keymap.set("n", "<leader><leader>q", "<cmd>qa<cr>", { noremap = true, silent = true, desc = "quit neovim" })
+vim.keymap.set("n", "<leader>!", function()
+    local choice = vim.fn.confirm("Really quit Neovim?", "&Yes\n&No", 2)
+    if choice == 1 then
+        vim.cmd("qa!")
+    end
+end, { desc = "Force quit Neovim" })
 
 -- clear search
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch\n", { noremap = true, silent = true, desc = "clear search" })
 
--- move focus
-vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-
--- split window
-vim.keymap.set("n", "<leader><C-h>", "<cmd>vsplit<CR> <C-w>h <cmd>enew<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader><C-j>", "<cmd>split<CR>  <C-w>j <cmd>enew<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader><C-k>", "<cmd>split<CR>  <C-w>k <cmd>enew<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader><C-l>", "<cmd>vsplit<CR> <C-w>l <cmd>enew<CR>", { noremap = true, silent = true })
-
--- resize window
-local resize = require("config.resize")
-vim.keymap.set("n", "<A-h>", resize.ResizeLeft, { noremap = true, silent = true })
-vim.keymap.set("n", "<A-j>", resize.ResizeDown, { noremap = true, silent = true })
-vim.keymap.set("n", "<A-k>", resize.ResizeUp, { noremap = true, silent = true })
-vim.keymap.set("n", "<A-l>", resize.ResizeRight, { noremap = true, silent = true })
-
--- terminal
+-- escape terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "escape terminal" })
+
+-- tab char
+vim.keymap.set("i", "<S-Tab>", "<C-v><Tab>")
 
 -- copy path
 vim.keymap.set("n", "<leader>yp", function()
@@ -42,26 +29,3 @@ vim.keymap.set("n", "<leader>yp", function()
     vim.fn.setreg("+", path)
     print("Copied file path: " .. path)
 end, { noremap = true, silent = true, desc = "copy current file path" })
-
--- markdown
--- bold
-vim.keymap.set("n", "<leader>mb", "diwi**<esc>pa**<esc>")
-vim.keymap.set("v", "<leader>mb", "di**<esc>pa**<esc>")
--- underline
-vim.keymap.set("n", "<leader>mu", "diwi<u><esc>pa</u><esc>")
-vim.keymap.set("v", "<leader>mu", "di<u><esc>pa</u><esc>")
--- math block
-vim.keymap.set("n", "<leader>mm", "diwi$<esc>pa$<esc>")
-vim.keymap.set("v", "<leader>mm", "di$<esc>pa$<esc>")
--- force break
-
--- toggle binary / hex
-vim.keymap.set("n", "<leader>b", function()
-    if vim.b.editHex then
-        vim.cmd("%!xxd -r")
-        vim.b.editHex = nil
-    else
-        vim.cmd("%!xxd")
-        vim.b.editHex = true
-    end
-end)
---
+++ lua/plugins/smart-splits.lua
@@ -0,0 +1,29 @@
+return {
+    "mrjones2014/smart-splits.nvim",
+    config = function()
+        local smart_splits = require("smart-splits")
+        smart_splits.setup()
+
+        -- split window
+        vim.keymap.set("n", "<leader><C-h>", "<cmd>vsplit<CR> <cmd>enew<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-j>", "<cmd>split<CR> <C-w>j <cmd>enew<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-k>", "<cmd>split<CR> <cmd>enew<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-l>", "<cmd>vsplit<CR> <C-w>l <cmd>enew<CR>", { noremap = true, silent = true })
+        -- resize
+        vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+        vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+        vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+        vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+        -- moving between splits
+        vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+        vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+        vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+        vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+        vim.keymap.set("n", "<C-->", require("smart-splits").move_cursor_previous)
+        -- swapping buffers between windows
+        vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+        vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+        vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+        vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+    end,
+}
