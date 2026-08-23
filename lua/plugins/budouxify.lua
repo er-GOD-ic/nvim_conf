871003a0f47d10439b3fbe13e11a215fad9c7c3e
ergodice <kamepower@outlook.jp>
2026-08-23 11:56:55 +0900

    update keymap

--- lua/plugins/budouxify.lua
+++ lua/plugins/budouxify.lua
@@ -1,22 +1,22 @@
 return {
     "atusy/budouxify.nvim",
-    dependencies = {
-        "atusy/budoux.lua",
-    },
+    dependencies = { "atusy/budoux.lua" },
     enabled = false,
     config = function()
         vim.keymap.set("n", "w", function()
-            local pos = require("budouxify.motion").find_forward({
-                head = true,
-            })
+            local pos = require("budouxify.motion").find_forward({ head = true })
             if pos then
                 vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
             end
         end)
         vim.keymap.set("n", "e", function()
-            local pos = require("budouxify.motion").find_forward({
-                head = false,
-            })
+            local pos = require("budouxify.motion").find_forward({ head = false })
+            if pos then
+                vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+            end
+        end)
+        vim.keymap.set("n", "b", function()
+            local pos = require("budouxify.motion").find_backward({ head = true })
             if pos then
                 vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
             end
