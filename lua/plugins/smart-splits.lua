b05c36a4d49fa294a4f2e1830234ce3e9936ae20
ergodice <kamepower@outlook.jp>
2026-08-24 15:39:23 +0900

    use more accurate commands

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -5,10 +5,10 @@ return {
         smart_splits.setup()
 
         -- split window
-        vim.keymap.set("n", "<leader><C-h>", "<cmd>vsplit<CR> <cmd>enew<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader><C-j>", "<cmd>split<CR> <C-w>j <cmd>enew<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader><C-k>", "<cmd>split<CR> <cmd>enew<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader><C-l>", "<cmd>vsplit<CR> <C-w>l <cmd>enew<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-h>", "<cmd>set nosplitright | vnew <cr>  ", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-j>", "<cmd>set splitbelow   | new  <cr>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-k>", "<cmd>set nosplitbelow | new  <cr>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader><C-l>", "<cmd>set splitright   | vnew <cr>", { noremap = true, silent = true })
         -- resize
         vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
         vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
@@ -19,7 +19,6 @@ return {
         vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
         vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
         vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-        vim.keymap.set("n", "<C-->", require("smart-splits").move_cursor_previous)
         -- swapping buffers between windows
         vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
         vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
